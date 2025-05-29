1158329ffe2cfab388409fbb1bdd1ce1db03f903
Ignacio Dominguez Martinez-Casanueva <ignacio.dominguezmartinez@telefonica.com>
2025-05-29 12:35:38 +0100

    Update project to new groupId io.github.candil-data-fabric

---
+++ src/test/java/io/github/candildatafabric/jnc/ExtendedElementTest.java
@@ -0,0 +1,99 @@
+package io.github.candildatafabric.jnc;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ExtendedElementTest {
+
+    YangElement parentElement;
+
+    private static final String ns = "http://test.com/ns/extendedtest";
+    private static final String parentName = "test-container";
+    private static final String childName = "test-child";
+
+    @Before
+    public void setUp() {
+        parentElement = new TestContainer();
+    }
+
+    @Test
+    public void testAddChildByName() throws Exception {
+        assertFalse("parent element has no child", parentElement.hasChildren());
+        String[] childrenNames = parentElement.childrenNames();
+        assertEquals("parent element has one child name", 1, childrenNames.length);
+        YangElement child = (YangElement) parentElement.addChild(childrenNames[0]);
+        assertTrue("parent element has children", parentElement.hasChildren());
+        YangElement copy = child.cloneContent(new TestChild());
+        assertTrue("child copy is a TestChild", copy instanceof TestChild);
+        parentElement.addChild(copy);
+        assertEquals("parent element has 2 children", 2, parentElement.getChildren().size());
+    }
+
+    public class TestContainer extends YangElement {
+
+        private static final long serialVersionUID = 1L;
+
+        public TestContainer() {
+            super(ns, parentName);
+        }
+
+        @Override
+        public String[] childrenNames() {
+            return new String[] {
+                    childName,
+                };
+        }
+
+        @Override
+        public String[] keyNames() {
+            return null;
+        }
+
+        @Override
+        public TestContainer cloneShallow() {
+            return (TestContainer)cloneShallowContent(new TestContainer());
+        }
+
+        public TestChild addTestChild() throws JNCException {
+            TestChild testChild = new TestChild();
+            insertChild(testChild, childrenNames());
+            return testChild;
+        }
+
+    }
+
+    public class TestChild extends YangElement {
+        private static final long serialVersionUID = 1L;
+
+        public TestChild() {
+            super(ns, childName);
+        }
+
+        @Override
+        public String[] childrenNames() {
+            return new String[] {
+                    "id",
+                    "value",
+                };
+        }
+
+        @Override
+        public String[] keyNames() {
+            return new String[] {
+                    "id",
+                };
+        }
+
+        @Override
+        public TestChild cloneShallow() {
+            return (TestChild)cloneShallowContent(new TestChild());
+        }
+
+    }
+
+
+}
---
+++ src/test/java/io/github/candildatafabric/jnc/YangEnumerationTest.java
@@ -0,0 +1,91 @@
+package io.github.candildatafabric.jnc;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class YangEnumerationTest {
+
+    private YangEnumeration one;
+    private YangEnumeration two;
+    private YangEnumeration spacy;
+    private String spacystr = "  leading  and trailing  ";
+
+    @Before
+    public void setUp() throws Exception {
+        one = new YangEnumeration("one", new String[] {"one"});
+        two = new YangEnumeration("two", new String[] {"two"});
+        spacy = new YangEnumeration("spacy", new String[] {"spacy"});
+    }
+
+    @Test
+    public void testCanEqual() {
+        assertTrue(one.canEqual(one));
+        assertTrue(one.canEqual(two));
+        assertTrue(one.canEqual(spacy));
+        assertTrue(spacy.canEqual(one));
+        assertFalse(one.canEqual("one"));
+        assertFalse(one.canEqual(null));
+    }
+
+    @Test
+    public void testYangEnumeration() throws YangException {
+        // Values set in setUp
+        assertEquals("one", one.value);
+        assertEquals("two", two.value);
+        assertEquals("spacy", spacy.value);
+
+        // Empty string not allowed
+        try {
+            spacy = new YangEnumeration("", new String[] {""});
+            fail("Expected YangException");
+        } catch (YangException e) {
+            assertEquals("empty string in enum value", e.opaqueData);
+        }
+        assertNotEquals("", spacy.value);
+        assertEquals("spacy", spacy.value);
+
+        // Leading and trailing spaces not allowed
+        try {
+            spacy = new YangEnumeration(spacystr, new String[] {spacystr});
+            fail("Expected YangException");
+        } catch (YangException e) {
+            assertEquals(spacystr, e.opaqueData);
+        }
+        assertNotEquals(spacystr, spacy.value);
+        assertEquals("spacy", spacy.value);
+
+        // No wsCollapse occurs
+        spacy = new YangEnumeration("leading  and trailing",
+                new String[] {"leading  and trailing"});
+        assertNotEquals("leading and trailing", spacy.value);
+        assertEquals("leading  and trailing", spacy.value);
+
+        // Null not allowed
+        try {
+            one = new YangEnumeration(null, null);
+            fail("Expected YangException");
+        } catch (YangException e) {
+            assertTrue(e.opaqueData instanceof NullPointerException);
+        }
+        try {
+            one = new YangEnumeration("hej", null);
+            fail("Expected YangException");
+        } catch (YangException e) {
+            assertEquals("no enum names provided", e.opaqueData);
+        }
+        try {
+            one = new YangEnumeration(null, new String[] {"hej"});
+            fail("Expected YangException");
+        } catch (YangException e) {
+            assertTrue(e.opaqueData instanceof NullPointerException);
+        }
+        assertEquals("one", one.value);
+    }
+
+}
